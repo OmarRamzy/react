15ef0c8d924897d73e27847e295f6fe08a909207
OmarRamzy <omar.ramzyi@gmail.com>
2019-04-16 17:09:11 +0200

    toogle persons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
-   
-  state={
-    persons:[
-      {name: 'Omar Ramzy' , age:"23"},
-      {name: 'Hussam Ashraf', age:"22"},
-      {name:'ahmed El-hadidy' , age:"24"}
+
+  state = {
+    persons: [
+      { name: 'Omar Ramzy', age: "23" },
+      { name: 'Hussam Ashraf', age: "22" },
+      { name: 'ahmed El-hadidy', age: "24" }
     ],
     otherAttribue: 'Test'
 
@@ -16,44 +16,57 @@ class App extends Component {
 
   switchNameHandler = (newName) => {
     //console.log("was clicked!");
-    this.setState({ 
-      persons:[
-        {name: 'Omar Adel Ramzy' , age:"23"},
-        {name: 'Hussam Ashraf', age:"25"},
-        {name: newName , age:"24"}
+    this.setState({
+      persons: [
+        { name: 'Omar Adel Ramzy', age: "23" },
+        { name: 'Hussam Ashraf', age: "25" },
+        { name: newName, age: "24" }
       ]
     });
   }
 
-  nameChangeHandler = (event) =>{
-    this.setState({ 
-      persons:[
-        {name: 'Omar Adel Ramzy' , age:"23"},
-        {name:  event.target.value, age:"25"},
-        {name: 'Ahmed ElHadidy' , age:"24"}
-      ]
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Omar Adel Ramzy', age: "23" },
+        { name: event.target.value, age: "25" },
+        { name: 'Ahmed ElHadidy', age: "24" }
+      ],
+      showPersons: false
+
     });
   }
 
+  togglePersonsHandler = () => {
+    const doseShow = this.state.showPersons ;
+    this.setState({showPersons: !doseShow}) ;
+  }
+
+  // <button className="btn btn-danger" onClick={this.switchNameHandler.bind(this , 'Ahmed')} >Switch names</button>
   render() {
     return (
-      <div className="container">
-      <h1>This is really working!</h1>
-      <button className="btn btn-danger" onClick={this.switchNameHandler.bind(this , 'Ahmed')} >Switch names</button>
-        <div className="row">
-          <div className="col-md-4">
-              <Person
-               click={this.switchNameHandler} 
-               name={this.state.persons[0].name} 
-               age={this.state.persons[0].age} ></Person>  
-              <Person  name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              changed={this.nameChangeHandler}
-              />
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age} ></Person>  
+      <div className="Person">
+        <h1 >This is really working!</h1>
+        <button className="btn btn-danger" onClick={this.togglePersonsHandler} >Switch names</button>
 
-          </div>
-        </div>
+        {
+        this.state.showPersons ===true?
+          <div>
+            <Person
+              click={this.switchNameHandler}
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} ></Person>
+            <Person name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.nameChangeHandler} />
+            <Person name={this.state.persons[2].name}
+              age={this.state.persons[2].age} ></Person>
+
+          </div> : null
+
+        }
+
+
       </div>
     );
   }
